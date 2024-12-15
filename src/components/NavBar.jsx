86a9b9c701259ad08a35583cc01b33d0bbc4b1d1
MACKENNADRIES <138457102+MACKENNADRIES@users.noreply.github.com>
2024-12-15 14:17:55 +1000

    update nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -16,7 +16,7 @@ function Navbar() {
             <nav className="navbar">
                 <ul className="navbar-list">
                     <li><a href="/home" className="navbar-link">Home</a></li>
-                    <li><a href="/discover" className="navbar-link">Discover</a></li>
+                    <li><a href="/all" className="navbar-link">Discover</a></li>
                     <li><a href="/create" className="navbar-link">Create</a></li>
                     <li><a href="/profile" className="navbar-link">Profile</a></li>
                     <li>
